115cc51432f9965ca038493deafe6544a4088cf2
Kiran <kiranindia1986@gmail.com>
2024-12-13 19:51:40 +0530

    push

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
+require('dotenv').config();
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require("./vida-uat-firebase-adminsdk-dqvcd-63b188be69.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
 });
 
 const app = express();
